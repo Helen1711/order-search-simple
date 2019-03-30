edeed27d837b0ddcbfb2c81ea0c9ae0cf009af42
Helen1711 <helen.mysiak@gmail.com>
2019-03-30 15:53:44 +0200

    Initial commit

---
+++ src/main/java/com/order/search/simple/OrderSearchSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.order.search.simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderSearchSimpleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderSearchSimpleApplication.class, args);
+    }
+
+}
